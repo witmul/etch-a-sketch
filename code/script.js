bc915b8ca55cd94ac1356e8d179b664f35afcb2d
Witalij <83119547+witmul@users.noreply.github.com>
2024-04-17 20:34:20 +0200

    feat: grid with flex

---
+++ code/script.js
@@ -0,0 +1,22 @@
+document.body.onload = addElement(100);
+
+
+function addElement(n) {
+  const numBoxes = n*n
+  const currentDiv = document.querySelector(".main-container");
+  const containerWidth = window.innerWidth
+  const borderSize = 1
+  const sqrSide = (containerWidth / n) - (borderSize*2) // calculated in px
+  currentDiv.style.width = `${containerWidth}px`
+  
+  for(i=0; i<numBoxes; i++){
+    const newDiv = document.createElement("div");
+    
+    
+    newDiv.className = "box";
+    newDiv.style.border = `${borderSize}px solid black`;
+    newDiv.style.width = `${sqrSide}px`;
+    newDiv.style.aspectRatio = "1/1"
+    currentDiv.appendChild(newDiv);
+  }
+}
